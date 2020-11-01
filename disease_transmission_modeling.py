2e6cb9ea16d3e249edd3a777a1155e07d1d0364f
Raahim <raahimsal@gmail.com>
2020-11-01 01:13:13 -0400

    + added SIR model output extration methods and cleaning of JHU dataset

--- disease_transmission_modeling.py
+++ disease_transmission_modeling.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout, Activation
 
 #Load data
 df = pd.read_csv('covid_cases.csv',index_col = "Date")#,parse_dates = True,index_col='date')
@@ -34,11 +36,9 @@ scaled_test = scaler.transform(test)
 
 n_input = 5  ## number of steps
 n_features = 1 ## number of features you want to predict (for univariate time series n_features=1)
-# generator = TimeseriesGenerator(scaled_train,scaled_train,length = n_input,batch_size=1)
-#
-# print("Scaled Train")
-# print(len(scaled_train))
-# print("Generator")
-# print(len(generator))
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, Activation
+generator = TimeseriesGenerator(scaled_train,scaled_train,length = n_input,batch_size=1)
+
+print("Scaled Train")
+print(len(scaled_train))
+print("Generator")
+print(len(generator))
